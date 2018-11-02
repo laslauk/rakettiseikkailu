edb6f7e646715ab4d37a16dd0944e0b8d18bc89d
lasse laukkanen <lasse.laukkanen@student.tut.fi>
2018-11-02 13:25:00 +0200

    Add thrust and rotation

--- Assets/scripts/Rocket.cs
+++ Assets/scripts/Rocket.cs
@@ -5,32 +5,49 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    public int multiplier = 150;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector2.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
-        if(Input.GetKey(KeyCode.A))
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            print("rotating Left");
-        } 
+            transform.Rotate(Vector3.forward * Time.deltaTime * multiplier);
+        }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("rotating right");
+            transform.Rotate(Vector3.forward * -1 * Time.deltaTime * multiplier);
         }
     }
+
 }
